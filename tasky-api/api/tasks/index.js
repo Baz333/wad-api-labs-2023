96d3d97ce3738cb9cdeecc5c39d0af05ae92a51a
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-11-28 12:18:08 +0000

    Mongo lab including Mongoose

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -1,4 +1,49 @@
 import express from 'express';
+import Task from './taskModel';
+
+const router = express.Router();
+
+//get all tasks
+router.get('/', async(req, res) => {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+});
+
+//create task
+router.post('/', async(req, res) => {
+    const task = await Task(req.body).save();
+    res.status(201).json(task);
+});
+
+//update task
+router.put('/:id', async(req, res) => {
+    if(req.body._id) delete req.body._id;
+    const result = await Task.updateOne({
+        _id: req.params.id,
+    }, req.body);
+    if(result.matchedCount) {
+        res.status(200).json({code:200, msg:'Task updated successfully'});
+    }  else {
+        res.status(404).json({code:404, msg:'Unable to find task'});
+    }
+});
+
+//delete task
+router.delete('/:id', async(req, res) => {
+    if(req.body._id) delete req.body._id;
+    const result = await Task.deleteOne({
+        _id: req.params.id,
+    });
+    if(result.deletedCount) {
+        res.status(204).json();
+    } else {
+        res.status(404).json({code:404, msg:'Unable to find task'});
+    }
+});
+
+export default router;
+
+/*import express from 'express';
 import { tasksData } from './tasksData';
 import {v4 as uuidv4} from 'uuid';
 
@@ -62,4 +107,4 @@ router.delete('/:id', (req, res) => {
     tasksData.total_results--;
 });
 
-export default router;
+export default router;*/
