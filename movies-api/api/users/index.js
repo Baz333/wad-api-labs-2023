35be96c983d46cf13d6016dedefb464927fb546b
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-12-10 16:25:34 +0000

    Added JWT

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
 import User from './userModel';
 
 const router = express.Router();
@@ -9,23 +10,39 @@ router.get('/', async (req, res) => {
     res.status(200).json(users);
 });
 
+// router.post('/', asyncHandler(async (req, res) => {
+//     if(req.query.action === 'register') {
+//         if(!req.body.username || !req.body.password) {
+//             return res.status(400).json({code: 400, msg: 'Please enter a username and a password'});
+//         }
+//         await User(req.body).save();
+//         res.status(201).json({
+//             code: 201,
+//             msg: 'Successfully created new user',
+//         });
+//     } else {
+//         const user = await User.findOne(req.body);
+//         if(!user) {
+//             return res.status(401).json({code: 401, msg: 'Authentication failed'});
+//         } else {
+//             return res.status(200).json({code: 200, msg: "Authentication successful", token: 'TEMPORARY_TOKEN'});
+//         }
+//     }
+// }));
+
 router.post('/', asyncHandler(async (req, res) => {
-    if(req.query.action === 'register') {
+    try {
         if(!req.body.username || !req.body.password) {
-            return res.status(400).json({code: 400, msg: 'Please enter a username and a password'});
+            return res.status(400).json({success: false, msg: 'Username and password are required'});
         }
-        await User(req.body).save();
-        res.status(201).json({
-            code: 201,
-            msg: 'Successfully created new user',
-        });
-    } else {
-        const user = await User.findOne(req.body);
-        if(!user) {
-            return res.status(401).json({code: 401, msg: 'Authentication failed'});
+        if(req.query.action === 'register') {
+            await registerUser(req, res);
         } else {
-            return res.status(200).json({code: 200, msg: "Authentication successful", token: 'TEMPORARY_TOKEN'});
+            await authenticateUser(req, res);
         }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({success: false, msg: 'Internal server error'});
     }
 }));
 
@@ -40,5 +57,24 @@ router.put('/:id', async (req, res) => {
         res.status(404).json({code: 404, msg: 'Unable to update user'});
     }
 });
+
+async function registerUser(req, res) {
+    await User.create(req.body);
+    res.status(201).json({success: true, msg: 'User successfully created'});
+};
+
+async function authenticateUser(req, res) {
+    const user = await User.findByUserName(req.body.username);
+    if(!user) {
+        return res.status(401).json({success: false, msg: 'Authentication failed. User not found'});
+    }
+    const isMatch = await user.comparePassword(req.body.password);
+    if(isMatch) {
+        const token = jwt.sign({username: user.userName}, process.env.SECRET);
+        res.status(200).json({success: true, token: 'BEARER ' + token});
+    } else {
+        res.status(401).json({success: false, msg: 'Wrong password'});
+    }
+}
 
 export default router;
