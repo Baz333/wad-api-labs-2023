222899f8df54f4e6fe13bb5412bcda883dfac009
Barry Nolan <76070900+Baz333@users.noreply.github.com>
2023-12-10 22:51:48 +0000

    Topic 12 - Get movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,6 +1,10 @@
 export const getMovies = async () => {
     const response = await  fetch(
-    	`https://api.themoviedb.org/3/discover/movie?${process.env.TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    	`http://localhost:8080/api/movies`, {
+			headers: {
+				'Authorization': window.localStorage.getItem('token')
+			}
+		}
     )
     return response.json()
 };
